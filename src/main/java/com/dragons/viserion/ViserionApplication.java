0da8625449562d1ed614a5e5518058b5060a24d6
marciomelo@gmail.com <marciomelo@gmail.com>
2017-11-25 09:46:54 -0300

    Stating project genereted by the Springboot

---
+++ src/main/java/com/dragons/viserion/ViserionApplication.java
@@ -0,0 +1,12 @@
+package com.dragons.viserion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViserionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViserionApplication.class, args);
+	}
+}
